139baf7e2bf61a2a6148003d98bcbca271cef9fb
McChesterson <o.a.onstott@gmail.com>
2023-09-30 12:22:16 -0700

    Added a speed-boost to be used when dashing

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,24 +4,56 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
     public Rigidbody2D rb;
     public Animator animator;
+
+    public float moveSpeed;
+    public float walkSpeed = 5f;
+    public float dashSpeed = 10f;
     Vector2 movement;
+
+    public bool dash = false;
+    public float dashLength = 3f;
+    public float dashRemaining;
     
     void Update()
     {
         //getting the input from w,a,s,d and arrows
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        movement = movement.normalized;
+
+        //dashing
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            dash = true;
+            dashRemaining = dashLength;
+            moveSpeed = dashSpeed;
+            Debug.Log("dashed");
+            
+        }
+        else
+        {
+            if (dashRemaining > 0)
+            {
+                dashRemaining -= 0.5f * Time.fixedDeltaTime;
+            }
+            else
+            {
+                dash = false;
+                moveSpeed = walkSpeed;
+            }
+            
+        }
 
         //setting what animation to use
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
-    }
-    void FixedUpdate()
-    {
+        animator.SetBool("isDashing", dash);
+        
+
+
         //flipping animation when walking to the left
         if (movement.x < -0.01f)
         {
@@ -31,7 +63,10 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.localScale = new Vector3(1, 1, 1);
         }
-        //code accually moving the Player
-        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+    void FixedUpdate()
+    {
+        //code moving the Player
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
