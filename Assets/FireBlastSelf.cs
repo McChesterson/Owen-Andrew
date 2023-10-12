4193d7d19b6e85cd7eed12d8e358921ee38a8733
McChesterson <o.a.onstott@gmail.com>
2023-10-11 22:27:03 -0700

    Made the FireBlast destroy itself after a certain distance

--- Assets/FireBlastSelf.cs
+++ Assets/FireBlastSelf.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class FireBlastSelf : MonoBehaviour
 {
-
+    public bool hasStarted = false;
+    public GameObject player;
+    public Vector3 playerPosition;
+    public float range;
 
     void Start()
     {
-        
+        player = GameObject.Find("Player");
+        playerPosition = player.transform.position;
     }
 
     void Update()
     {
-        
+        if ((playerPosition - gameObject.transform.position).magnitude > range)
+        {
+            Destroy(gameObject);
+        }
     }
 }
